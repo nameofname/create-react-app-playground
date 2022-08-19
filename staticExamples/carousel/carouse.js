007973c8fe8d34f84b887de553a666b7e28578c5
nameofname <nameofemail@gmail.com>
2022-08-18 20:22:08 -0400

    static js example of carousel

---
+++ staticExamples/carousel/carouse.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const img = document.querySelector('.viewer-img');
+    const title = document.querySelector('.title');
+    const loading = document.querySelector('.loading');
+    const left = document.querySelector('.left');
+    const right = document.querySelector('.right');
+    let pointer = 0;
+    let dogs;
+
+    function movePointer(inc) {
+        pointer = Math.max(Math.min(pointer + inc, dogs.length - 1), 0);
+        img.setAttribute('src', dogs[pointer].url);
+        title.innerText = dogs[pointer].title;
+    }
+
+    function buildCarousel() {
+        // img.setAttribute('src', dogs[0]);
+        movePointer(0);
+        loading.classList.add('hidden');
+        img.classList.remove('hidden');
+        left.addEventListener('click', () => movePointer(-1));
+        right.addEventListener('click', () => movePointer(1));
+    }
+
+    dogapi().then(d => {
+        dogs = d;
+        buildCarousel();
+    });
+});
---
+++ staticExamples/carousel/dogapi.js
@@ -0,0 +1,25 @@
+/**
+ * Get cute dog pictures
+ * @param {number} length
+ * @returns {Promise<Array<{ title: string, url:string }>}
+ */
+function dogapi () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          url: "http://cdn.akc.org/content/article-body-image/golden_puppy_dog_pictures.jpg",
+          title: "hey bro",
+        },
+        {
+          url: "https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__340.jpg",
+          title: "im yo dog brooooo",
+        },
+        {
+          url: "https://cdn3.photostockeditor.com/t/1501/dog-chocolate-labrador-retriever-puppy-on-green-grass-puppy-puppy-image.jpg",
+          title: "bitch ass bitches.",
+        },
+      ]);
+    }, 500);
+  });
+};
